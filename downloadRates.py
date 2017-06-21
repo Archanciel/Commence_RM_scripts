4285af296b4bc716bf7a480a90d39852a0db9eb3
Archanciel <jp.schnyder@gmail.com>
2017-06-21 23:46:32 +0200

    add realtime quotes obtention

--- downloadRates.py
+++ downloadRates.py
@@ -14,7 +14,7 @@ OUTPUT_FILE_NAME = "c:/temp/output.txt"
 def getXRate():
 	quandl.ApiConfig.api_key = "TzTJNAxsD45RySvjBduE"
 
-	statusMsg = 'SUCCESS - getting BTC/USD and USD/CHF rates at date {0} successful'
+	statusMsg = 'SUCCESS - getting BTC/USD ({0}) and USD/CHF ({1}) rates at date {2} successful !'
 
 	#getting input parms, i.e transactionDate, btcTransactionAmount, btcFeesAmount
 	with open(INPUT_PARMS_FILE_PATH, 'r') as f:
@@ -28,7 +28,6 @@ def getXRate():
 	transDateDDMMYYYY = lines[0]
 	btcTransactionAmount = float(lines[1])
 	btcFeesAmount = float(lines[2])
-	statusMsg = statusMsg.format(transDateDDMMYYYY.replace('\n', ''))
 
 	transDateComponents = transDateDDMMYYYY.split('.')
 	transDateYYYY = int(transDateComponents[2])
@@ -36,17 +35,41 @@ def getXRate():
 	transDateDD = int(transDateComponents[0])
 
 	transDateObj = datetime.date(transDateYYYY, transDateMM, transDateDD)
-	dateStart = transDateObj
-	dateEnd = dateStart
-	dataBtc = quandl.get(["BCHARTS/KRAKENUSD.4"], start_date=dateStart, end_date=dateEnd, returns="numpy")
-	btcUsdClosePrice = dataBtc[0][1]
 
-	usdTotalTransAmount = (btcTransactionAmount + btcFeesAmount) * btcUsdClosePrice
+	if transDateObj == datetime.date.today():
+		#getting realtime rates
+		btcUsdPrice = getRealTimeXRates('BTC','USD')
+		usdChfRate = getRealTimeXRates('USD','CHF')
+	else:
+		# getting historical rates
+		btcUsdPrice, usdChfRate = getHistoricalXRatesForDate(transDateObj)
 
-	cr = CurrencyRates()
-	usdChfRate = cr.get_rate('USD', 'CHF', transDateObj)
+	usdTotalTransAmount = (btcTransactionAmount + btcFeesAmount) * btcUsdPrice
+
+	statusMsg = statusMsg.format(btcUsdPrice, usdChfRate, clearEOL(transDateDDMMYYYY))
 
 	writeOutputParms(statusMsg, usdChfRate, usdTotalTransAmount)
+
+
+def getHistoricalXRatesForDate(dateObj):
+	dateStart = dateObj
+	dateEnd = dateStart
+	dataBtc = quandl.get(["BCHARTS/KRAKENUSD.4"], start_date=dateStart, end_date=dateEnd, returns="numpy")
+	if dataBtc.size > 0:
+		btcUsdClosePrice = dataBtc[0][1]
+	else:
+		statusMsg = "ERROR - BTC/USD close price not found for date {0} !".format(dateStart)
+		writeOutputParms(statusMsg, '0', '0')
+		sys.exit(1)
+
+	cr = CurrencyRates()
+	usdChfRate = cr.get_rate('USD', 'CHF', dateObj)
+
+	return float(btcUsdClosePrice), float(usdChfRate)
+
+
+def clearEOL(line):
+	return line.replace('\n', '')
 
 
 def writeOutputParms(statusMsg, usdChfRate, usdTotalTransAmount):
@@ -56,5 +79,45 @@ def writeOutputParms(statusMsg, usdChfRate, usdTotalTransAmount):
 		f.write(str(usdChfRate) + "\n")
 
 
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+
+def getRealTimeXRates(baseCur, targetCur):
+    '''
+    Scrap the bitcoin rate in the passed currency
+    Usage: getBTCRate("CHF"), getBTCRate("USD")
+    :return: bitcoin rate in USD
+    '''
+
+    url = "http://markets.businessinsider.com/currencies/realtime-chart/" + baseCur.lower() + '-' + targetCur.lower()
+    #req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    req = Request(url)
+    page = urlopen(req).read()
+    soup = BeautifulSoup(page,"lxml")
+    # print(soup.prettify())
+    rateStr = soup.find("span",{'class':'push-data price'}).string
+    rateStr = cleanRate(rateStr)
+    return float(rateStr)
+
+
+def cleanRate(rateStr):
+    '''
+    remove thousands divider from passed
+    rate string and return cleaned string
+    '''
+    return rateStr.replace(',','')
+
+
 if __name__ == '__main__':
     getXRate()
+    baseCur = 'BTC'
+    targetCur = 'USD'
+    print(baseCur + '/' + targetCur + ': ' + str(getRealTimeXRates(baseCur,targetCur)))
+
+    baseCur = 'BTC'
+    targetCur = 'CHF'
+    print(baseCur + '/' + targetCur + ': ' + str(getRealTimeXRates(baseCur,targetCur)))
+
+    baseCur = 'USD'
+    targetCur = 'CHF'
+    print(baseCur + '/' + targetCur + ': ' + str(getRealTimeXRates(baseCur,targetCur)))
